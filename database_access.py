e4b72fd7edfc2ce4fb1809eda6282b90f6973345
rory-holmes <roryholmes2@gmail.com>
2025-02-19 13:22:49 +1300

    Craeted methods for creating and connecting to db

--- database_access.py
+++ database_access.py
@@ -1,3 +1,34 @@
-def create_database():
-    pass
+import mysql.connector
+import logging
 
+def create_database():
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="darkestt",
+    port=3306,
+  )
+  mycursor = mydb.cursor()
+  try:
+    mycursor.execute("CREATE DATABASE librarydb")
+    logging.info("Created librarydb")
+  except Exception as e:
+    logging.error(f"Error creating db: {e}")
+
+def connectdb():
+  global mycursor
+  try:
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="darkestt",
+      port=3306,
+      database="librarydb"
+    )
+    logging.info(f"Connected: {mydb.is_connected()}")
+    mycursor = mydb.cursor()
+  except Exception as e:
+    logging.error(f"Error connecting to librarydb: {e}")
+  
+
+connectdb()
